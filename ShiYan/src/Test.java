9582bd6554267fed0dee19108719d34e9ecc2619
小米 <@DESKTOP-Q1EFK47>
2020-06-29 14:45:12 +0800

    王东1

--- ShiYan/src/Test.java
+++ ShiYan/src/Test.java
@@ -4,7 +4,7 @@ public class Test {
 	public static void main(String[] args) throws InterruptedException {
 	Solution solution = new Solution();
 	System.out.println(solution.isHappy(1));
-		//修改了
+		//王东修改了
 	}
 }
 
